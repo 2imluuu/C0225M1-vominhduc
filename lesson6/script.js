73b157e601ac3c76ab9bf0f1a9a67200a3d9ad22
vmduc9x <vmduc9x@gmail.com>
2025-03-26 17:40:46 +0700

    sinh bang cuu chuong

--- lesson6/script.js
+++ lesson6/script.js
@@ -1,1 +1,9 @@
-alret("xin chào");
+// script.js
+document.getElementById('myButton').addEventListener('click', function() {
+    var message = "Hello, world!";
+    console.log(mesage);
+});
+function calculateSum(a, b) {
+    return a + b;
+}
+calculateSum(5, "10");
